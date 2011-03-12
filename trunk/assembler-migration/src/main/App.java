3c581ff422cc0ba25bcd8eb58e2a2b39be0bbe3d
niktrk@gmail.com <niktrk@gmail.com@5392641c-92ad-e376-c369-ac5d8dad59e9>
2011-03-12 16:47:41 +0000

    Added main package and main application class. It seems that bin directory must exist on repository so it is created again.

---
+++ trunk/assembler-migration/src/main/App.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Main application class.
+ * 
+ * @author Nikola Trkulja
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Let je gaz");
+	}
+
+}
