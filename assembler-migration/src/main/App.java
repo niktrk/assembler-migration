f864b2941cc68197c50f8aebb5b74d0c827130cd
niktrk <niktrk@gmail.com>
2011-05-31 23:13:13 +0000

    App class finished.

--- assembler-migration/src/main/App.java
+++ assembler-migration/src/main/App.java
@@ -1,5 +1,6 @@
 package main;
 
+
 /**
  * Main application class.
  * 
@@ -10,32 +11,28 @@ public class App {
 
 	public static void main(String[] args) throws Exception {
 
-		// start remove
-		args = new String[1];
-		args[0] = "src/asm/macrobug.asm";
-		// end remove
+		Scanner scanner = new Scanner("src/asm/macrobug.asm");
+		Parser parser = new Parser(scanner);
+		System.out.println(parser.parse());
 
-		if (args.length != 0) {
-			String filename = args[0];
-			if (filename.endsWith(".asm")) {
-				String outputFilename = filename.substring(0, filename.lastIndexOf('.')).concat(
-						".wsl");
-				Scanner scanner = new Scanner(filename);
-				Parser parser = new Parser(scanner);
-				// PrintWriter output = new PrintWriter(new FileWriter(outputFilename));
-				// output.print(parser.parse());
-				// output.close();
-
-				// start remove
-				System.out.println(parser.parse());
-				// end remove
-
-			} else {
-				System.out.println("Input file extension must be '.asm'.");
-			}
-		} else {
-			System.out.println("No input file specified.");
-		}
+		// if (args.length != 0) {
+		// String filename = args[0];
+		// if (filename.endsWith(".asm")) {
+		// String outputFilename = filename.substring(0, filename.lastIndexOf('.')).concat(
+		// ".wsl");
+		// Scanner scanner = new Scanner(filename);
+		// Parser parser = new Parser(scanner);
+		// PrintWriter output = new PrintWriter(new FileWriter(outputFilename));
+		// output.print(parser.parse());
+		// output.close();
+		// System.out.println("Parsing and code generation complited successfully.");
+		// System.out.println("You can view results in file: " + outputFilename);
+		// } else {
+		// System.out.println("Input file extension must be '.asm'.");
+		// }
+		// } else {
+		// System.out.println("No input file specified.");
+		// }
 	}
 
 }
