b74e0df63ed686600de9e10ea338f389becb3dea
niktrk@gmail.com <niktrk@gmail.com@5392641c-92ad-e376-c369-ac5d8dad59e9>
2011-05-31 20:44:08 +0000

    Added char array (string) support, fixed scanning and array index parsing.

--- trunk/assembler-migration/src/main/App.java
+++ trunk/assembler-migration/src/main/App.java
@@ -12,7 +12,7 @@ public class App {
 
 		// start remove
 		args = new String[1];
-		args[0] = "src/asm/main.asm";
+		args[0] = "src/asm/helloworld.asm";
 		// end remove
 
 		if (args.length != 0) {
